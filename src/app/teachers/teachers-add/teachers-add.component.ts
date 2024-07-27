e624debbf126921dceba14dbde255f7e0082d208
ManashDholey <chottu.dholey52@gmail.com>
2024-07-27 16:34:23 +0530

    commit for teachers component

--- src/app/teachers/teachers-add/teachers-add.component.ts
+++ src/app/teachers/teachers-add/teachers-add.component.ts
@@ -1,10 +1,174 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Gender } from 'src/app/models/ui-models/gender.model';
+import { Teachers } from 'src/app/models/ui-models/teachers.model';
+import { TeacherService } from '../teacher.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GenderService } from 'src/app/services/gender.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-teachers-add',
   templateUrl: './teachers-add.component.html',
   styleUrls: ['./teachers-add.component.css']
 })
-export class TeachersAddComponent {
+export class TeachersAddComponent implements OnInit{
+  teacherId: string | null | undefined;
+  teacher: Teachers = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
+    mobile: 0,
+    genderId: '',
+    profileImageUrl: '',
+    gender: {
+      id: '',
+      description: ''
+    },
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: ''
+    }
+  };
+
+  isNewStudent = false;
+  header = '';
+  displayProfileImageUrl = '';
+
+  genderList: Gender[] = [];
+
+  @ViewChild('teacherDetailsForm') teacherDetailsForm?: NgForm;
+
+  constructor(private readonly teacherService: TeacherService,
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar,
+    private router: Router) { }
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      (params) => {
+        this.teacherId = params.get('id');
+        if (this.teacherId) {
+          if (this.teacherId.toLowerCase() === 'Add'.toLowerCase()) {
+            // -> new Student Functionality
+            this.isNewStudent = true;
+            this.header = 'Add New Student';
+            this.setImage();
+          } else {
+            // -> Existing Student Functionality
+            this.isNewStudent = false;
+            this.header = 'Edit Student';
+            this.teacherService.getById(this.teacherId)
+              .subscribe(
+                (successResponse) => {
+                  this.teacher = successResponse;
+                  this.setImage();
+                },
+                (errorResponse) => {
+                  this.setImage();
+                }
+              );
+          }
+          this.genderService.getGenderList()
+            .subscribe(
+              (successResponse) => {
+                this.genderList = successResponse;
+              }
+            );
+        }
+      }
+    );
+  }
+
+  onUpdate(): void {
+    if (this.teacherDetailsForm?.form.valid) {
+      this.teacherService.update(this.teacher.id, this.teacher)
+        .subscribe(
+          (successResponse) => {
+            // Show a notification
+            this.snackbar.open('Teacher updated successfully', undefined, {
+              duration: 2000
+            });
+          },
+          (errorResponse) => {
+            // Log it
+            console.log(errorResponse);
+          }
+        );
+    }
+  }
+
+  onDelete(): void {
+    this.teacherService.delete(this.teacher.id)
+      .subscribe(
+        (successResponse) => {
+          this.snackbar.open('Teacher deleted successfully', undefined, {
+            duration: 2000
+          });
+          setTimeout(() => {
+            this.router.navigateByUrl('teachers');
+          }, 2000);
+        },
+        (errorResponse) => {
+          // Log
+        }
+      );
+  }
+
+  onAdd(): void {
+    if (this.teacherDetailsForm?.form.valid) {
+      // Submit form date to api
+      this.teacherService.add(this.teacher)
+        .subscribe(
+          (successResponse) => {
+            this.snackbar.open('Teacher added successfully', undefined, {
+              duration: 2000
+            });
+            setTimeout(() => {
+              this.router.navigateByUrl('teachers');
+            }, 2000);
+          },
+          (errorResponse) => {
+            // Log
+            console.log(errorResponse);
+          }
+        );
+    }
+  }
+
+  uploadImage(event: any): void {
+    if (this.teacherId) {
+      const file: File = event.target.files[0];
+      this.teacherService.uploadImage(this.teacher.id, file)
+        .subscribe(
+          (successResponse) => {
+            this.teacher.profileImageUrl = successResponse;
+            this.setImage();
+            // Show a notification
+            this.snackbar.open('Profile Image Updated', undefined, {
+              duration: 2000
+            });
+          },
+          (errorResponse) => {
+
+          }
+        );
+    }
+  }
+
+  private setImage(): void {
+    if (this.teacher.profileImageUrl) {
+      this.displayProfileImageUrl = this.teacherService.getImagePath(this.teacher.profileImageUrl);
+    } else {
+      // Display a default
+      this.displayProfileImageUrl = '/assets/user.png';
+    }
+  }
+
 
 }
--- src/app/teachers/teachers-list/teachers-list.component.ts
+++ src/app/teachers/teachers-list/teachers-list.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Teachers } from 'src/app/models/ui-models/teachers.model';
+import { TeacherService } from '../teacher.service';
 
 @Component({
   selector: 'app-teachers-list',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./teachers-list.component.css']
 })
 export class TeachersListComponent {
+  teachers: Teachers[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender', 'edit'];
+  dataSource: MatTableDataSource<Teachers> = new MatTableDataSource<Teachers>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+  filterString = '';
+
+  constructor(private teachesService: TeacherService) { }
+
+  ngOnInit(): void {
+    // Fetch Students
+    this.teachesService.getAll()
+      .subscribe(
+        (successResponse) => {
+          this.teachers = successResponse;
+          this.dataSource = new MatTableDataSource<Teachers>(this.teachers);
+
+          if (this.matPaginator) {
+            this.dataSource.paginator = this.matPaginator;
+          }
+
+          if (this.matSort) {
+            this.dataSource.sort = this.matSort;
+          }
+        },
+        (errorResponse) => {
+          console.log(errorResponse);
+        }
+      );
+  }
+
+  filterTeachers() {
+    this.dataSource.filter = this.filterString.trim().toLowerCase();
+  }
+
+
+
 
 }
